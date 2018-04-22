db9c3b1d5c81a42a7eaf6ea52df5cb2c277a792a
Kevin Tong <kejuntong@gmail.com>
2018-04-22 19:45:35 -0400

    first commit

---
+++ app/src/main/java/kejuntong/com/samplemoduleapp/LoginActivity.java
@@ -0,0 +1,43 @@
+package kejuntong.com.samplemoduleapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by kejuntong on 2018-04-22.
+ */
+
+public class LoginActivity extends Activity {
+
+    Button loginButton;
+    TextView registerButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
+
+        registerButton = findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/kejuntong/com/samplemoduleapp/RegisterActivity.java
@@ -0,0 +1,92 @@
+package kejuntong.com.samplemoduleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by kejuntong on 2018-04-22.
+ */
+
+public class RegisterActivity extends Activity {
+    final static String TAG = "RegisterActivity";
+    private FirebaseAuth mAuth;
+
+    Button registerButton;
+    EditText email;
+    EditText password;
+    EditText passwordAgain;
+
+    ProgressBar spinner;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        passwordAgain = findViewById(R.id.password_again);
+        registerButton = findViewById(R.id.register);
+
+        spinner = findViewById(R.id.progress_bar);
+
+        setRegisterButton();
+    }
+
+    private void setRegisterButton(){
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                spinner.setVisibility(View.VISIBLE);
+
+                if (email.getText() == null || password.getText() == null){
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                spinner.setVisibility(View.GONE);
+
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(RegisterActivity.this, "Authentication success.",
+                                            Toast.LENGTH_SHORT).show();
+//                            updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                                }
+                            }
+                        });
+            }
+        });
+
+
+    }
+}
